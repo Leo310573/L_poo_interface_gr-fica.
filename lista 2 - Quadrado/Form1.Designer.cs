05cf3b3c681508c799a51b6f7147966875ac6d0e
Leo310573 <104110486+Leo310573@users.noreply.github.com>
2025-09-29 19:54:28 -0300

    Add files via upload

---
+++ lista 2 - Quadrado/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lista_2___Quadrado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAresta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblÁrea = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAresta
+            // 
+            this.txtAresta.Location = new System.Drawing.Point(281, 100);
+            this.txtAresta.Name = "txtAresta";
+            this.txtAresta.Size = new System.Drawing.Size(100, 20);
+            this.txtAresta.TabIndex = 0;
+            this.txtAresta.Text = "0";
+            this.txtAresta.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(378, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Digitar valor da aresta";
+            // 
+            // lblÁrea
+            // 
+            this.lblÁrea.Location = new System.Drawing.Point(281, 139);
+            this.lblÁrea.Name = "lblÁrea";
+            this.lblÁrea.Size = new System.Drawing.Size(75, 23);
+            this.lblÁrea.TabIndex = 4;
+            this.lblÁrea.Text = "Calcular";
+            this.lblÁrea.UseVisualStyleBackColor = true;
+            this.lblÁrea.Click += new System.EventHandler(this.lblCalular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblÁrea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAresta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAresta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button lblÁrea;
+    }
+}
+
---
+++ lista 2 - Quadrado/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lista_2___Quadrado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("==Área do quadrado (aresta)==");
+
+            AreaQuadrado aq;
+            aq = new AreaQuadrado();
+
+            do
+            {
+                Console.Write("Digite o valor da Aresta: ");
+                aq.setAresta(double.Parse(txtAresta.Text));
+            }
+            while (aq.getAresta() <= 0);
+
+            aq.calcular();
+            lblÁrea.Text = aq.getResultado().ToString();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Aresta: {0}", aq.getAresta());
+            Console.Write("Área do Quadrado = {0}", aq.getResultado());
+        }
+
+ 
+
+    }
+}
